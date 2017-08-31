9e3de52aab00b5ecc58277f2551eed8a8a791548
Marcin <marcinster@gmail.com>
2017-08-31 18:22:05 +0200

    Wzorzec ADAPTER initial commit

---
+++ src/ADAPTER/Main.java
@@ -0,0 +1,35 @@
+package ADAPTER;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        DzikaKaczka kaczka = new DzikaKaczka();
+        DzikiIndyk indyk = new DzikiIndyk();
+        IndykAdapter ia = new IndykAdapter(indyk);
+        KaczkaAdapter ka = new KaczkaAdapter(kaczka);
+
+//        ArrayList<IKaczka> ptaki = new ArrayList<>();
+//        ptaki.add(kaczka);
+//        ptaki.add(ia);
+//        uruchomKwakanieILatanie(ptaki);
+        ArrayList<IIndyk> ptaki = new ArrayList<>();
+        ptaki.add(indyk);
+        ptaki.add(ka);
+        uruchomGulganieILatanie(ptaki);
+    }
+
+    public static void uruchomKwakanieILatanie(ArrayList<IKaczka> ptaki) {
+        for (IKaczka kaczka : ptaki) {
+            kaczka.kwacz();
+            kaczka.lataj();
+        }
+    }
+
+    public static void uruchomGulganieILatanie(ArrayList<IIndyk> ptaki) {
+        for (IIndyk indyk : ptaki) {
+            indyk.gulgocze();
+            indyk.lataj();
+        }
+    }
+}

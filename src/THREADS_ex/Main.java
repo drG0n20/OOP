bd57482dea981242a0320f2e3a47c48d9591d54a
Br0n50n <marcinster@gmail.com>
2017-09-04 23:17:56 +0200

    Thread Customer - Cook

--- src/THREADS_ex/Main.java
+++ src/THREADS_ex/Main.java
@@ -1,4 +1,34 @@
 package THREADS_ex;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Processor processor = new Processor();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.customer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.cook();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
 }

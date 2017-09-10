4efa4ad696f1da396c34983868cd55fce0056a69
Br0n20n <marcinster@gmail.com>
2017-09-10 23:03:42 +0200

    powtWiad 0709 Zad3 not finished

---
+++ src/PowtorzenieWiad07092017/Zad3_LosowanieLiczb/Main.java
@@ -0,0 +1,18 @@
+//package PowtorzenieWiad07092017.Zad3_LosowanieLiczb;
+//
+//import java.util.Random;
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        int randomNumersAmount = Integer.parseInt(args[0]);
+//        Scanner sc = new Scanner();
+//        Random rand = new Random();
+//        int number = 0;
+//        int max = 0;
+//        int min = 0;
+//
+//
+//    }
+//
+//}
